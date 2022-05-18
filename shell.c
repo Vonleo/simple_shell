f80e24790d58a74dd42badb4c0a9e73efac85f0f
Collince2021 <okotocollince@gmail.com>
2022-05-18 06:45:40 +0300

    Siimple shell

--- shell.c
+++ shell.c
@@ -1,27 +1,49 @@
-#include "olaf.h"
-
+#include "shell.h"
 /**
- * main - Entry point of the program.
- * @ac: The number of parameters passed to the executable file. In the case
- * this variable will not be used.
- * @av: The name of the program.
- * Return: Always 0.
+ * main - Holberton Shell
+ * @ac: counter of argumments to receive
+ * @av: pointer to args to receive
+ * Return: 0 on success.
  */
-int main(__attribute__((unused)) int ac, char **av)
+int main(int ac, char **av)
 {
-	char *line;
-	size_t size;
-	int command_counter;
+	char *string = NULL;
+	pid_t child;
+	size_t MaxSize = 1024;
+	int ret = 0, errors = 1;
+	char **toktok = NULL;
+	char *exi = "exit\n";
+	char *envi = "env\n";
+	char *validate = "\n";
+	(void)ac;
 
-	command_counter = 0;
-	signal(SIGINT, SIG_IGN);
-	do {
-		command_counter++;
-		line = NULL;
-		size = 0;
-		parse_line(line, size, command_counter, av);
-
-	} while (1);
-
+	signal(SIGINT, handler);
+	while (1)
+	{
+		if (isatty(STDIN_FILENO) == 1)
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
+		ret = getline(&string, &MaxSize, stdin);
+		ctrl_d(ret, string);
+		if (_strcmp(string, envi) == 0)
+		{
+			_printenv();
+		}
+		if (_strcmp(string, exi) == 0)
+		{
+			free(string);
+			exit(EXIT_SUCCESS);
+		}
+		if (_strcmp(string, validate) == 0)
+			continue;
+		child = fork();
+		if (fork_process(child, string, toktok) == -1)
+		{
+			validate_input(&string, av[0], errors);
+			_free(toktok);
+			exit(EXIT_FAILURE);
+		}
+		errors++;
+	}
+	free(string);
 	return (0);
 }
